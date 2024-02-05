6179bdb94293e9d89b0379b46da7b1c5bc3d4eae
anshul-ch <anshul.chaudhary160603@gmail.com>
2024-02-05 10:28:59 +0530

    R data structures, Data Frames

---
+++ Feb5.R
@@ -0,0 +1,25 @@
+# create a data frame of company names, price and country.
+n1 <- as.character(readline("Enter name p1 : "))
+n2 <- as.character(readline("Enter p2 : "))
+n3 <- as.character(readline("Enter p3 : "))
+v1 <- as.numeric(readline("Enter v1 : "))
+v2 <- as.numeric(readline("Enter v2 : "))
+v3 <- as.numeric(readline("Enter v3 : "))
+c1 <- as.character(readline("Enter c1"))
+c2 <- as.character(readline("Enter c2"))
+c3 <- as.character(readline("Enter c3"))
+name <- c(n1,n2,n3)
+price <- c(v1,v2,v3)
+country <-c(c1,c2,c3)
+dataframe1 <- data.frame("Name" = name, "Country"  =country, "Price" = price)
+print(dataframe1)
+print(dataframe1$Price)
+dataframe1 <- edit(dataframe1)
+colnames(dataframe1)[1] <- "Product_Name"
+colnames(dataframe1) <- c("Name","Origin", "Original_Price")
+summary(dataframe1)
+View(dataframe1)
+str(dataframe1)
+
+# Columns are called as Variables in DataSet.
+# Rows are called as Observations or Records in DataSet.
