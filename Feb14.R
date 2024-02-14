53833f96365a461ebdf690ed02617b59a36db2d3
anshul-ch <anshul.chaudhary160603@gmail.com>
2024-02-14 09:24:38 +0530

    Save data frame into file

--- Feb14.R
+++ Feb14.R
@@ -16,4 +16,16 @@ for (i in 1:5) {                                           # loop to take the in
 rollno <- as.numeric(rollno)                                # convert the rollno array/vector to numeric
 cgpa <- as.numeric(cgpa)                                    # convert the cgpa array/vector to numeric
 dataframe1 <- data.frame("Rollno" = rollno, "Std_Name" =name, "CGPA"=cgpa)       # create a dataframe
+
+# add new data to the dataframe by rbind and cbind
+
+newdata <- data.frame("Rollno" = 22, "Std_Name" = "Riya","CGPA" = 7.0)           # create a new row
+dataframe1 <- rbind(dataframe1,newdata)                                          # add the new row to the dataframe
+
+# add new column to the dataframe
+city <- c("Mumbai","Pune","Delhi","Kolkata","Chennai","Bangalore")
+dataframe1 <- cbind(dataframe1, "City" = city)                                  # add the new column to the dataframe
 print(dataframe1)
+
+write.csv(dataframe1, file = "dataframe1.csv")                                  # write the dataframe to a csv file
+write.csv(dataframe1, file = "dataframe1.xlsx")                               # write the dataframe to an excel file
