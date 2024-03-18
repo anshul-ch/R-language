906c8d93dd5c874b83ee2bf5930a8080f6317747
anshul-ch <anshul.chaudhary160603@gmail.com>
2024-03-18 10:53:58 +0530

    Sqldf and plot

--- Mar18.R
+++ Mar18.R
@@ -21,4 +21,34 @@
   View(sqldf("Select * from dataset where City like 'H%'"))         # starting with h.
   View(sqldf("Select * from dataset where City like 'H%N'"))        # starting with h and ending with n.
 
+  # Aggregate functions:- sum, min, max, avg, count, group by,
+  #                       order by, distinct, limit, where, like, in, between, is null, is not null.
+
+  View(sqldf("select sum(sales) as Total_Sales from dataset"))      # overall sum of sales of dataset.
+  View(sqldf("select sum(sales) as Total_Sales from dataset where Category='Office Supplies'"))
+  View(sqldf("select sum(sales), max(sales) from dataset"))
+  View(sqldf("select * from dataset group by Segment"))
+  View(sqldf("Select Region from dataset
+             where Profit == (select max(Profit) from dataset) "))   # region with max profit
+  View(sqldf("Select Region, sum(Sales) from dataset GROUP BY Region"))          # Region with their total sales.
+  View(sqldf("select distinct State from dataset order by State ASC"))      # unique state from dataset.
+
+  # plot function
+
+  plot(dataset$Sales)
+  plot(dataset$Sales, pch = 13)               # pch to change the type of circle or bullet.
+  plot(x =dataset$Profit, y=dataset$Sales, pch = 10,
+       col = "red", xlab = "Profit", ylab = "Total Sales")
+
+  plot(x =dataset$Profit, y=dataset$Sales, pch = 10,
+       col = "red", xlab = "Profit",
+       ylab = "Total Sales", xlim = c(0,3000))   # value of x axis only between 0 and 3000
+
+  plot(x =dataset$Profit, y=dataset$Sales, pch = 10,
+       col = "red", xlab = "Profit",
+       ylab = "Total Sales", ylim = c(0,1200))  # value of y axis only between 0 and 1200
+
+  plot(x =dataset$Profit, y=dataset$Sales, pch = 10,
+       col = "red", xlab = "Profit",
+       ylab = "Total Sales", main = "Sales VS Profit")  # main to give name to plot
 }
